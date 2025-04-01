1f5736cf191445b888a3bedbf4554e9d06e42d02
Bosun Egberinde <bosunski@gmail.com>
2025-04-01 22:34:43 +0200

    Update error ErrorEvent

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
+export type ErrorCode = 500 | 400 | 401 | 403 | 404 | 409 | 422 | 429 | 5000;
+
 export class ErrorEvent {
 	public constructor(
-		public readonly code: number,
+		public readonly code: ErrorCode,
 		public readonly message: string,
 		public readonly raw: object = {}
 	) {}
