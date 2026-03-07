6cca6dc8f5678cd4af73e8988e99b6fd031a99dd
Bosun Egberinde <15146515+bosunski@users.noreply.github.com>
2026-03-07 23:31:31 +0100

    Add more git API

--- src/git.ts
+++ src/git.ts
@@ -11,23 +11,41 @@ export interface GitRef {
   ref: string;
 }
 
+export interface GitStatus {
+  initialized: boolean;
+  clean: boolean;
+  branch: string;
+  ref: string;
+}
+
+export interface GitCheckoutResult extends GitRef {
+  branch: string;
+  created: boolean;
+}
+
 export interface GitEvents {}
 
 export interface GitActions {
-  'git.checkpoint': Action<{ author: string; message: string; branch: string }, GitRef>;
+  'git.checkpoint': Action<{ author: string; message: string; branch: string; allowEmpty?: boolean }, GitRef>;
+  'git.checkout': Action<{ branch: string; create?: boolean }, GitCheckoutResult>;
   'git.log': Action<{ ref: string }, GitLog[]>;
   'git.sync': Action<
     { url: string; author: string; ref: string; token?: string; direction?: 'pull' | 'push' | 'both'; force?: boolean },
     GitRef
   >;
   'git.restore': Action<{ ref: string }, GitRef>;
+  'git.status': Action<object, GitStatus>;
 }
 
 export default class Git {
   constructor(protected okra: NotebookInstance) {}
 
-  public checkpoint(author: string, message: string, branch = 'main') {
-    return this.okra.invoke('git.checkpoint', { author, message, branch });
+  public checkpoint(author: string, message: string, branch = 'main', allowEmpty = false) {
+    return this.okra.invoke('git.checkpoint', { author, message, branch, allowEmpty });
+  }
+
+  public checkout(branch: string, create = true) {
+    return this.okra.invoke('git.checkout', { branch, create });
   }
 
   public sync(
@@ -47,5 +65,9 @@ export default class Git {
 
   public restore(ref: string) {
     return this.okra.invoke('git.restore', { ref });
+  }
+
+  public status() {
+    return this.okra.invoke('git.status', {});
   }
 }
